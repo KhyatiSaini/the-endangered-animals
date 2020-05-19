0e6ce3513dc4c32e2a25e53d502ad0437e6cd2c1
Shwetagurnani <sainikhyati2001@gmail.com>
2020-05-19 21:55:14 +0530

    Files added

---
+++ app/src/main/java/com/example/theanimals/HomeActivity.java
@@ -0,0 +1,45 @@
+package com.example.theanimals;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(selectedListener);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId())
+                    {
+                        case R.id.nav_home:
+                            selectedFragment = new Home_Fragment();
+                            break;
+                        case R.id.nav_facts:
+                            selectedFragment = new Facts_Fragment();
+                            break;
+                        case R.id.nav_stats:
+                            selectedFragment = new Statistics_Fragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+}
---
+++ app/src/main/java/com/example/theanimals/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.theanimals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
